db688e9e4b4273c13c149ec545508c6763ce9f9c
Aleksandr Barmin <abarmin@outlook.com>
2022-03-17 09:52:25 +0000

    Add error handling

--- src/main/java/dev/abarmin/velosiped/checker/service/RepositoryTestsUpdater.java
+++ src/main/java/dev/abarmin/velosiped/checker/service/RepositoryTestsUpdater.java
@@ -1,13 +1,17 @@
 package dev.abarmin.velosiped.checker.service;
 
+import dev.abarmin.velosiped.checker.domain.RepositoryFork;
 import dev.abarmin.velosiped.checker.repository.ForksRepository;
+import java.util.Optional;
 import java.util.stream.StreamSupport;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
  * @author Aleksandr Barmin
  */
+@Slf4j
 @Component
 public class RepositoryTestsUpdater {
   @Autowired
@@ -18,7 +22,19 @@ public class RepositoryTestsUpdater {
 
   public void update() {
     StreamSupport.stream(forksRepository.findAll().spliterator(), false)
-        .map(repositoryTestsLoader::updateTests)
+        .map(this::updateAndHandleExceptions)
+        .filter(Optional::isPresent)
+        .map(Optional::get)
         .forEach(forksRepository::save);
+  }
+
+  private Optional<RepositoryFork> updateAndHandleExceptions(final RepositoryFork fork) {
+    try {
+      final RepositoryFork updatedFork = repositoryTestsLoader.updateTests(fork);
+      return Optional.of(updatedFork);
+    } catch (Exception e) {
+      log.error("Can't update fork {}, error: ", fork.getName(), e);
+      return Optional.empty();
+    }
   }
 }
